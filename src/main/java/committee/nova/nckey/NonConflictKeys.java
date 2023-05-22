c56e8e2bede53b776cb60159011893f6bc1a87a4
MikhailTapio <mikhailtapio13468@163.com>
2023-05-22 16:35:56 +0800

    Compatibility text

--- src/main/java/committee/nova/nckey/NonConflictKeys.java
+++ src/main/java/committee/nova/nckey/NonConflictKeys.java
@@ -1,7 +1,13 @@
 package committee.nova.nckey;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("nckey")
 public class NonConflictKeys {
+    public NonConflictKeys() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> "", (net, save) -> true));
+    }
 }
